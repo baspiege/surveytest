12ea5966700ace9f9eaa83437df926071d603d05
spiegel121 <spiegel121@gmail.com>
2014-08-24 16:51:26 +0000

    Language

---
+++ src/surveytest/data/LanguageAdd.java
@@ -0,0 +1,23 @@
+package surveytest.data;
+
+import surveytest.data.model.Language;
+import java.util.Date;
+import javax.jdo.PersistenceManager;
+
+public class LanguageAdd {
+
+    public static Language execute(Language aLanguage) {
+
+        PersistenceManager pm=null;
+        try {
+            pm=PMF.get().getPersistenceManager();
+
+            pm.makePersistent(aLanguage);
+        } finally {
+            if (pm!=null) {
+                pm.close();
+            }
+        }
+        return aLanguage;
+    }
+}
