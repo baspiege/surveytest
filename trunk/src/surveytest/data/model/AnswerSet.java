2e4aa91ab5e29b7d95becf6f665a0f2fea22142c
spiegel121@gmail.com <spiegel121@gmail.com@5e42f6d9-9549-0ecb-771c-6ea0cb0ae837>
2014-08-30 19:35:15 +0000

    Answers.

---
+++ trunk/src/surveytest/data/model/AnswerSet.java
@@ -0,0 +1,54 @@
+package surveytest.data.model;
+
+import com.google.appengine.api.datastore.Key;
+import java.io.Serializable;
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable(identityType = IdentityType.APPLICATION, detachable="true")
+
+public class AnswerSet implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+    private Key key;
+
+    @Persistent
+    private String description;
+    
+    @Persistent
+    private long surveyId;
+
+    /**
+     * Constructor.
+     *
+     */
+    public AnswerSet()
+    {
+    }
+
+    public Key getKey() {
+        return key;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String aDescription) {
+        description=aDescription;
+    }
+    
+    public long getSurveyId() {
+        return surveyId;
+    }
+    
+    public void setSurveyId(long aSurveyId) {
+        surveyId=aSurveyId;
+    }
+}
