4c058248857d038a67a0559a67ad4264ab767cbc
spiegel121@gmail.com <spiegel121@gmail.com@5e42f6d9-9549-0ecb-771c-6ea0cb0ae837>
2014-09-18 03:15:43 +0000

    Answer page.

---
+++ trunk/src/surveytest/data/AnswerTextGetAll.java
@@ -0,0 +1,46 @@
+package surveytest.data;
+
+import surveytest.data.model.Answer;
+import surveytest.data.model.AnswerText;
+import java.util.ArrayList;
+import java.util.List;
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+
+public class AnswerTextGetAll {
+
+    public static List<AnswerText> execute(Long aAnswerId, Long aStart, String aSortBy) {
+        PersistenceManager pm=null;
+        List<AnswerText> results=null;
+        try {
+            pm=PMF.get().getPersistenceManager();
+            Query query=null;
+            try {
+                query = pm.newQuery(AnswerText.class);
+                query.setFilter("answerId==answerIdParam");
+                query.declareParameters("long answerIdParam");
+
+                // Sorting
+                // if (aSortBy==null || aSortBy.equalsIgnoreCase("name")){
+                //    query.setOrdering("noteLowerCase ASC");
+                //}
+
+                //query.setRange(aStart, aStart+10);
+
+                results = (List<AnswerText>) query.execute(aAnswerId);
+
+                // Touch object to get data.  Size method triggers the underlying database call.
+                results.size();
+            } finally {
+                if (query!=null) {
+                    query.closeAll();
+                }
+            }
+        } finally {
+            if (pm!=null) {
+                pm.close();
+            }
+        }
+        return results;
+    }
+}
