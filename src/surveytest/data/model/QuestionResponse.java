fa10c9def4e8c5d85e9f4db64859449b02b69c70
spiegel121 <spiegel121@gmail.com>
2014-09-29 01:20:29 +0000

    Save survey response.

---
+++ src/surveytest/data/model/QuestionResponse.java
@@ -0,0 +1,98 @@
+package surveytest.data.model;
+
+import com.google.appengine.api.datastore.Key;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.NotPersistent;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable(identityType = IdentityType.APPLICATION, detachable="true")
+
+public class QuestionResponse implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+    private Key key;
+    
+    @Persistent
+    private long surveyId;
+    
+    @Persistent
+    private long surveyResponseId;
+    
+    @Persistent
+    private long questionId;    
+    
+    @Persistent
+    private String questionText;
+    
+    @Persistent
+    private long answerId;
+    
+    @Persistent
+    private String answerText;
+
+    public QuestionResponse()
+    {
+    }
+
+    public Key getKey() {
+        return key;
+    }
+    
+    public String getAnswerText() {
+        return answerText;
+    }
+
+    public void setAnswerText(String aAnswerText) {
+        answerText=aAnswerText;
+    }
+    
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public void setQuestionText(String aQuestionText) {
+        questionText=aQuestionText;
+    }
+    
+    public long getResponseSurveyId() {
+        return surveyResponseId;
+    }
+    
+    public void setSurveyResponseId(long aSurveyResponseId) {
+        surveyResponseId=aSurveyResponseId;
+    }
+    
+    public long getSurveyId() {
+        return surveyId;
+    }
+    
+    public void setSurveyId(long aSurveyId) {
+        surveyId=aSurveyId;
+    }
+    
+    public long getAnswerId() {
+        return answerId;
+    }
+    
+    public void setAnswerId(long aAnswerId) {
+        answerId=aAnswerId;
+    }  
+    
+    public long getQuestionId() {
+        return questionId;
+    }
+    
+    public void setQuestionId(long aQuestionId) {
+        questionId=aQuestionId;
+    }      
+
+}
---
+++ src/surveytest/data/model/SurveyResponse.java
@@ -0,0 +1,56 @@
+package surveytest.data.model;
+
+import com.google.appengine.api.datastore.Key;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.NotPersistent;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable(identityType = IdentityType.APPLICATION, detachable="true")
+
+/**
+ * Survey response.
+ */
+public class SurveyResponse implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+    private Key key;
+    
+    @Persistent
+    private long surveyId;
+    
+    @Persistent
+    private long languageId;
+
+    public SurveyResponse()
+    {
+    }
+
+    public Key getKey() {
+        return key;
+    }
+        
+    public long getSurveyId() {
+        return surveyId;
+    }
+    
+    public void setSurveyId(long aSurveyId) {
+        surveyId=aSurveyId;
+    }
+    
+    public long getLanguageId() {
+        return languageId;
+    }
+    
+    public void setLanguageId(long aLanguageId) {
+        languageId=aLanguageId;
+    }
+}
