ef951fddc48aa90750a0e135814f2baffe394b16
spiegel121 <spiegel121@gmail.com>
2014-10-05 21:16:18 +0000

    Delete language and code cleanup.

--- src/surveytest/data/QuestionTextGetAll.java
+++ src/surveytest/data/QuestionTextGetAll.java
@@ -9,7 +9,7 @@ import javax.jdo.Query;
 
 public class QuestionTextGetAll {
 
-    public static List<QuestionText> execute(Long aQuestionId, Long aStart, String aSortBy) {
+    public static List<QuestionText> execute(Long aQuestionId) {
         PersistenceManager pm=null;
         List<QuestionText> results=null;
         try {
@@ -19,13 +19,6 @@ public class QuestionTextGetAll {
                 query = pm.newQuery(QuestionText.class);
                 query.setFilter("questionId==questionIdParam");
                 query.declareParameters("long questionIdParam");
-
-                // Sorting
-                // if (aSortBy==null || aSortBy.equalsIgnoreCase("name")){
-                //    query.setOrdering("noteLowerCase ASC");
-                //}
-
-                //query.setRange(aStart, aStart+10);
 
                 results = (List<QuestionText>) query.execute(aQuestionId);
 
@@ -44,7 +37,7 @@ public class QuestionTextGetAll {
         return results;
     }
     
-    public static List<QuestionText> executeBySurveyId(Long aSurveyId, Long aStart, String aSortBy) {
+    public static List<QuestionText> executeBySurveyId(Long aSurveyId) {
         PersistenceManager pm=null;
         List<QuestionText> results=null;
         try {
@@ -55,14 +48,35 @@ public class QuestionTextGetAll {
                 query.setFilter("surveyId==surveyIdParam");
                 query.declareParameters("long surveyIdParam");
 
-                // Sorting
-                // if (aSortBy==null || aSortBy.equalsIgnoreCase("name")){
-                //    query.setOrdering("noteLowerCase ASC");
-                //}
-
-                //query.setRange(aStart, aStart+10);
-
                 results = (List<QuestionText>) query.execute(aSurveyId);
+
+                // Touch object to get data.  Size method triggers the underlying database call.
+                results.size();
+            } finally {
+                if (query!=null) {
+                    query.closeAll();
+                }
+            }
+        } finally {
+            if (pm!=null) {
+                pm.close();
+            }
+        }
+        return results;
+    }
+    
+    public static List<QuestionText> executeByLanguageId(Long aLanguageId) {
+        PersistenceManager pm=null;
+        List<QuestionText> results=null;
+        try {
+            pm=PMF.get().getPersistenceManager();
+            Query query=null;
+            try {
+                query = pm.newQuery(QuestionText.class);
+                query.setFilter("languageId==languageIdParam");
+                query.declareParameters("long languageIdParam");
+
+                results = (List<QuestionText>) query.execute(aLanguageId);
 
                 // Touch object to get data.  Size method triggers the underlying database call.
                 results.size();
