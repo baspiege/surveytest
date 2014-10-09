03a38617c4a6228e7de758039a84e7d0801af677
spiegel121 <spiegel121@gmail.com>
2014-10-09 03:37:20 +0000

    Move edit methods to separate class.

---
+++ src/surveytest/utils/EditUtils.java
@@ -0,0 +1,75 @@
+package surveytest.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+import javax.servlet.http.HttpServletRequest;
+
+public class EditUtils {
+
+    public static String EDITS="edits";
+   
+    /**
+    * Add edit.
+    *
+    * @param aRequest Servlet Request
+    * @param aEditMessage edit message
+    */
+    public static void addEdit(HttpServletRequest aRequest, String aEditMessage) {
+        getEdits(aRequest).add(aEditMessage);
+    }
+
+    /**
+    * Add edit.
+    *
+    * @param aRequest Servlet Request
+    * @param aKey key in Text ResourceBundle
+    */
+    public static void addEditUsingKey(HttpServletRequest aRequest, String aKey) {
+        ResourceBundle bundle = ResourceBundle.getBundle("Text");
+        getEdits(aRequest).add(bundle.getString(aKey));
+    }
+
+    /**
+    * Get the edits.
+    *
+    * @return a list of edits
+    */
+    public static List<String> getEdits(HttpServletRequest aRequest) {
+        List<String> edits=(List<String>)aRequest.getAttribute(EDITS);
+        if (edits==null) {
+            edits=new ArrayList<String>();
+            aRequest.setAttribute(EDITS,edits);
+        }
+
+        return edits;
+    }
+
+    /**
+    * Has edits.
+    *
+    * @param aRequest Servlet Request
+    * @return a boolean indicating if there are edits
+    */
+    public static boolean hasEdits(HttpServletRequest aRequest) {
+        boolean hasEdits=false;
+        List<String> edits=(List<String>)aRequest.getAttribute(EDITS);
+        if (edits!=null && edits.size()>0) {
+            hasEdits=true;
+        }
+
+        return hasEdits;
+    }
+
+    /**
+    * Remove edits.
+    *
+    * @param aRequest Servlet Request
+    */
+    public static void removeEdits(HttpServletRequest aRequest) {
+        List<String> edits=(List<String>)aRequest.getAttribute(EDITS);
+        if (edits!=null && edits.size()>0) {
+            edits.clear();
+        }
+    }
+}
